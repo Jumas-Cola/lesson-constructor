0aa79a40cc18f711d18d444fc655687c6293e008
Jumas-Cola <kbbyfl91@gmail.com>
2019-04-07 00:44:25 +0300

    upd file saving

--- index.php
+++ index.php
@@ -157,14 +157,15 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+    <script src="FileSaver.min.js"></script>
     <script>
       function saveDoc(){
-        var a = document.createElement("a");
         var text = $(document.querySelectorAll('#lesson_list > ul > li > .card-body > .full_text')).text();
-        a.setAttribute("href", "data:text/plain;charset=utf-8," + text);
-        a.setAttribute("download", "lesson_list.doc");
-        a.click();
+        var filename = "lesson_list.doc";
+        var blob = new Blob([text], {
+          type: "text/plain;charset=utf-8"
+        });
+        saveAs(blob, filename);
       }
 
       function checkDownlBtn(){
@@ -173,6 +174,6 @@
           document.getElementById('save_btn').setAttribute("hidden", true);
         }
       }
-  </script>
+    </script>
   </body>
 </html>
